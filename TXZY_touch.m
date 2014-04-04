66672a2ee76ae7ad6f8da9138227f873237f1657
mathewzilla <mathewhevans@gmail.com>
2014-04-04 15:12:04 +0100

    merged remote to local

--- TXZY_touch.m
+++ TXZY_touch.m
@@ -8,7 +8,7 @@ cd box_1;
 for i = 1:5;
     for j = 1:3;
         filename = (['box_sample_' num2str(i) '_f' num2str(j) '.txt']);
-        lseval load num2str(filename)
+        eval load num2str(filename)
         
     end
 end
