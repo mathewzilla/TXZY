d441c7b97d35d1d9537a26c8472ac1fd98819d71
mathewzilla <mathewhevans@gmail.com>
2014-04-10 22:11:13 +0100

    now with box and glass data

--- TXZY_touch.m
+++ TXZY_touch.m
@@ -1,4 +1,4 @@
-% Script for loading data from the TXZY robot, looking at the data and,
+ % Script for loading data from the TXZY robot, looking at the data and,
 % eventually, classifying the data
 
 
@@ -12,3 +12,17 @@ for i = 1:5;
         
     end
 end
+
+
+load box_1/box_sample_1_f1.txt
+X = box_sample_1_f1;
+
+clear box_sample_1_f1
+
+load box_2/box_sample_1_f1.txt
+Y = box_sample_1_f1;
+
+subplot(2,2,1); plot(X/max(max(X)));
+subplot(2,2,2); hist(X/max(max(X)));
+subplot(2,2,3); plot(Y/max(max(Y)));
+subplot(2,2,4); hist(Y/max(max(Y)));
