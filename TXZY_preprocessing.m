964c8cf3b26a10f147cf6afea9f5e41e991a8888
mathewzilla <mathewhevans@gmail.com>
2014-04-11 11:28:40 +0100

    data collating done

--- TXZY_preprocessing.m
+++ TXZY_preprocessing.m
@@ -2,9 +2,24 @@
 % classification.
 
 %% BALL and CYLINDER dataset_10_04_2014
+clear all
+objects = {'ball_1','ball_2','ball_3','cylinder_1','cylinder_2','cylinder_3'};
+datatype = {'position','pressure','pressure','pressure'};
+finger = {'f1','f1','f2','f3'};
+TXZY_data = cell(6,4,3); % 6 objects, 4 types (one position, 3 finger pressures), 3 samples. All files are X by variable length
+for i = 1:6; % objects
+    for j = 1:4 % datatype
+        for k = 1:3 %sample
+            eval ([' load dataset_10_04_2014/' objects{i} '/' objects{i} '_' datatype{j} '_sample_' num2str(k) '_' finger{j} '.txt;']);
+            eval ([' X = ' objects{i} '_' datatype{j} '_sample_' num2str(k) '_' finger{j} ';']);
+            eval ([' clear ' objects{i} '_' datatype{j} '_sample_' num2str(k) '_' finger{j} ';']);
+            TXZY_data{i,j,k} = X;
+        end
+    end
+end
 
-
-dataset_10_04_2014/ball_1/
+while(0);
+%dataset_10_04_2014/ball_1/
 ball_1_position_sample_1_f1.txt
 ball_1_position_sample_2_f1.txt
 ball_1_position_sample_3_f1.txt
@@ -18,7 +33,7 @@ ball_1_pressure_sample_3_f1.txt
 ball_1_pressure_sample_3_f2.txt
 ball_1_pressure_sample_3_f3.txt
 
-dataset_10_04_2014/ball_2/
+%dataset_10_04_2014/ball_2/
 ball_2_position_sample_1_f1.txt
 ball_2_position_sample_2_f1.txt
 ball_2_position_sample_3_f1.txt
@@ -33,7 +48,7 @@ ball_2_pressure_sample_3_f2.txt
 ball_2_pressure_sample_3_f3.txt
 
 
-dataset_10_04_2014/ball_3/
+%dataset_10_04_2014/ball_3/
 ball_3_position_sample_1_f1.txt
 ball_3_position_sample_2_f1.txt
 ball_3_position_sample_3_f1.txt
@@ -47,7 +62,7 @@ ball_3_pressure_sample_3_f1.txt
 ball_3_pressure_sample_3_f2.txt
 ball_3_pressure_sample_3_f3.txt
 
-dataset_10_04_2014/cylinder_1/
+%dataset_10_04_2014/cylinder_1/
 cylinder_1_position_sample_1_f1.txt
 cylinder_1_position_sample_2_f1.txt
 cylinder_1_position_sample_3_f1.txt
@@ -61,7 +76,7 @@ cylinder_1_pressure_sample_3_f1.txt
 cylinder_1_pressure_sample_3_f2.txt
 cylinder_1_pressure_sample_3_f3.txt
 
-dataset_10_04_2014/cylinder_2/
+%dataset_10_04_2014/cylinder_2/
 cylinder_2_position_sample_1_f1.txt
 cylinder_2_position_sample_2_f1.txt
 cylinder_2_position_sample_3_f1.txt
@@ -75,7 +90,7 @@ cylinder_2_pressure_sample_3_f1.txt
 cylinder_2_pressure_sample_3_f2.txt
 cylinder_2_pressure_sample_3_f3.txt
 
-dataset_10_04_2014/cylinder_3/
+%dataset_10_04_2014/cylinder_3/
 cylinder_3_position_sample_1_f1.txt
 cylinder_3_position_sample_2_f1.txt
 cylinder_3_position_sample_3_f1.txt
@@ -88,3 +103,4 @@ cylinder_3_pressure_sample_2_f3.txt
 cylinder_3_pressure_sample_3_f1.txt
 cylinder_3_pressure_sample_3_f2.txt
 cylinder_3_pressure_sample_3_f3.txt
+end
