a2321fea3266d01d0d8ea13c43ad22b9ba207b8e
mathewzilla <mathewhevans@gmail.com>
2014-04-11 14:43:25 +0100

    prep code written

--- TXZY_class_prep.m
+++ TXZY_class_prep.m
@@ -1,75 +1,72 @@
 function [data_train_c,data_test,indObject] = TXZY_class_prep(data,smoo,deriv)
 % Preparation of datasets into appropriate training and test sets.
 
+% ALL available data is used for training and testing, as real test will be
+% online classification. This would need to change for a paper.
+
 %% TXZY BALL and CYLINDER dataset_10_04_2014
 % 6 objects, 4 types (1 position, 3 finger pressures), 3 trials, 6 contacts
-    [ob,ty,tr,co] = size(data);     %  6,4,3,6 | 2001,2
-    
-    coi = 1:co;        % 1 to 6 contacts
-    obi = 1:1:ob;      % 1 to 6 objects
-    tyi = 1:1:ty;      % 1 to 4 data types
-    tri = 1:1:tr;      % 1 to 3 trials
-    
-    nc = coi*tri;      % total sample number per set - 18 (contacts * trials)
-    rc = 1:nc;         % 1 to 18
-    subs = 1:17;       % subset for training set. Trial 1 will be left out
-    st = 151;          % length of peak-aligned segment
-    
+[ob,ty,tr,co] = size(data);     %  6,4,3,6 | 2001,2
 
-    for c1 = obi
-        for c2 = tyi
-            for i = tri
-                for j = coi
+coi = 1:co;        % 1 to 6 contacts
+obi = 1:1:ob;      % 1 to 6 objects
+tyi = 1:1:ty;      % 1 to 4 data types
+tri = 1:1:tr;      % 1 to 3 trials
+
+nc = co*tr;        % total sample number per set - 18 (contacts * trials)
+rc = 1:nc;         % 1 to 18
+% subs = 1:17;       % subset for training set. Trial 1 will be left out
+st = 151;          % length of peak-aligned segment
+
+
+for c1 = obi
+    for c2 = tyi
+        for i = tri
+            for j = coi
                 
                 %% OPTION: Take derivatives
                 if deriv == 1;
-                    data{c1,c2,i} = diff(data{c1,c2,i});
-                    data{c1,c2,i}(nt(1)-1,:) = data{c1,c2,i}(nt(1)-2,:);% Fill in missing end value
-                    data{c1,c2,i}(nt(1),:) = data{c1,c2,i}(nt(1)-1,:);
+                    data{c1,c2,i,j} = diff(data{c1,c2,i});
                 end
                 
-                if deriv == 2;
-                    data{c1,c2,i} = diff(diff(data{c1,c2,i}));
-                    data{c1,c2,i}(nt(1)-2,:) = data{c1,c2,i}(nt(1)-3,:);% Fill in missing end value
-                    data{c1,c2,i}(nt(1)-1,:) = data{c1,c2,i}(nt(1)-2,:);
-                    data{c1,c2,i}(nt(1),:) = data{c1,c2,i}(nt(1)-1,:);
-                end
                 
                 %% OPTION: Smooth
                 if smoo == 1;
                     g = @(n,si) exp(-(-n:n).^2/(2*si^2))/sum( exp((-n:n).^2/(2*si^2)) );
-                    data{c1,c2,i} = filter(g(50,15)/sum(g(50,15)),1,data{c1,c2,i});
+                    data{c1,c2,i,j} = filter(g(50,15)/sum(g(50,15)),1,data{c1,c2,i,j});
                 end
                 
-                end
             end
         end
     end
-    
+end
 
-    %% Concatenated files, if classifier needs it
-    
-    c = 0;
-    for c1 = rc1
-        for c2 = rc2
-            c = c + 1; data_train_c{c} = [];
-            for k = subs
-                rt = t(c1,c2,k+1)+(1:st);
-                data_train_c{c} = [data_train_c{c};data{c1,c2,k+1}(rt,1)]; % was (rt,1)
+
+%% Concatenated files
+
+c = 0;
+for c2 = tyi       % CONTACT TYPE FIRST, SO FIRST 6 ARE PRESSURE 6 DIFF OBJECTS
+    for c1 = obi
+        c = c + 1; data_train_c{c} = [];
+        for i = tri
+            for j = coi
+                data_train_c{c} = [data_train_c{c};data{c1,c2,i,j}];
+                %indObject(c) = c1;
                 
             end
         end
     end
-    
-    %% Test data: truncate
-    c = 0;
-    for c1 = rc1
-        for c2 = rc2
-            c = c + 1; data_test{c} = [];
-            for l = 1
-                rt = t(c1,c2,l)+(1:st);
-                data_test{c} = [data_test{c};data{c1,c2,l}(rt,1)]; % was (rt,1)
-                test(c) = c;
+end
+
+%% Test data: truncate
+c = 0;
+for c2 = tyi
+    for c1 = obi
+        for i = tri
+            for j = coi
+                c = c + 1; data_test{c} = [];
+                data_test{c} = data{c1,c2,i,j}; 
+                indObject(c) = c1;
             end
         end
     end
